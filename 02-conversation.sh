cbced60fb332d80075cd0bdb170785458031c6da
Kuruva Dastagiri <kuruvadastagiri@Kuruvas-MacBook-Air.local>
2025-11-03 22:38:24 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Abhi : : Hi Jaanvi how are you ? "
+echo "Jaanvi :: Hi Abhi i am fine how are you ? "
+echo "Abhi :: i am fine Jaanvi. "
+echo "Jaanvi :: Where are you now ? "
+echo "Abhi :: I am in peddanelatu. "
