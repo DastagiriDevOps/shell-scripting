cd7f69591d3d94ea35a419ddc75a08799f7a773f
Kuruva Dastagiri <kuruvadastagiri@Kuruvas-MacBook-Air.local>
2025-11-05 17:16:09 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+Persion1=$1
+Persion2=$2
+
+echo "$Persion1 : : Hi $Persion2 how are you ? "
+echo "$Persion2 :: Hi $Persion1 i am fine how are you ? "
+echo "$Persion1 :: i am fine $Persion2. "
+echo "$Persion2 :: Where are you now ? "
+echo "$Persion1 :: I am in peddanelatu. "
