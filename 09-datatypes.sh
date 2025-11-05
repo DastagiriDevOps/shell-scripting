dc60740441bfe168e82fc0a09493195f0c97dda0
Kuruva Dastagiri <kuruvadastagiri@pool-100-0-8-97.bstnma.fios.verizon.net>
2025-11-05 18:29:31 +0530

    variables

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -6,3 +6,10 @@ Number2=250
 Sum=$(($Number1+$Number2))
 
 echo "The Sum is : $Sum"
+
+Group=("Jaanvi" "Abhi" "Teja")
+
+echo "all group ${Group[@]}"
+echo " first persion ${Group[0]}
+echo " scecond persion ${Group[1]}
+echo " third persion ${Group[2]}
