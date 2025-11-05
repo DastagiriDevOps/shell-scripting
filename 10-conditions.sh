58ca560197d5a428cd9e79957de318e3be9f7c98
Kuruva Dastagiri <kuruvadastagiri@pool-100-0-8-97.bstnma.fios.verizon.net>
2025-11-05 18:48:57 +0530

    variables

---
+++ 10-conditions.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+Number=$1
+
+if [ $Number -lt 15 ] then
+
+echo "Given number $Number is less than to 15"
+
+elif [ $Number -eq 15] then
+
+echo "Given number $Number is eqal to 15"
+
+else
+
+echo "Given number $Number is greter than to 15"
+
+fi
