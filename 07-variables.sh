f3082338ec82cc1ad552f6e4b1a4e880828b955f
Kuruva Dastagiri <kuruvadastagiri@pool-100-0-8-97.bstnma.fios.verizon.net>
2025-11-05 18:12:22 +0530

    variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-START_TIME=$(date)
+START_TIME=$(date +%s)
 
 sleep 10
 
-END_TIME=$(date)
+END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($START_TIME-$END_TIME))
 
